ed104e0c1acb880de5683f93d576ffc7de1e37f3
maf <Programmer00001H@gmail.com>
2025-11-10 21:27:40 +0800

    remove redundant iterators

--- array.hpp
+++ array.hpp
@@ -4,53 +4,49 @@
 #include<span>
 #include"iterable.hpp"
 namespace cppp{
-    namespace detail{
-        template<typename T>
-        class fixed_array{
-            std::span<T> buf;
-            public:
-                fixed_array(std::size_t sz) : buf(new T[sz],sz){}
-                fixed_array(const fixed_array&) = delete;
-                fixed_array(fixed_array&& other) noexcept : buf(std::exchange(other.buf,std::span<T>())){}
-                fixed_array& operator=(const fixed_array&) = delete;
-                fixed_array& operator=(fixed_array&& other) noexcept{
-                    buf = std::exchange(other.buf,std::span<T>());
-                    return *this;
-                }
-                T& operator[](std::size_t i) noexcept{
-                    return buf[i];
-                }
-                const T& operator[](std::size_t i) const noexcept{
-                    return buf[i];
-                }
-                T* data() noexcept{
-                    return buf.data();
-                }
-                const T* data() const noexcept{
-                    return buf.data();
-                }
-                std::size_t size() const noexcept{
-                    return buf.size();
-                }
-                using iterator = T*;
-                using const_iterator = const T*;
-                T* begin() noexcept{
-                    return buf.data();
-                }
-                const T* begin() const noexcept{
-                    return buf.data();
-                }
-                T* end() noexcept{
-                    return buf.data()+buf.size();
-                }
-                const T* end() const noexcept{
-                    return buf.data()+buf.size();
-                }
-                ~fixed_array(){
-                    delete[] buf.data();
-                }
-        };
-    }
     template<typename T>
-    using fixed_array = reverse_iterable<const_iterable<detail::fixed_array<T>>>;
+    class fixed_array{
+        std::span<T> buf;
+        public:
+            fixed_array(std::size_t sz) : buf(new T[sz],sz){}
+            fixed_array(const fixed_array&) = delete;
+            fixed_array(fixed_array&& other) noexcept : buf(std::exchange(other.buf,std::span<T>())){}
+            fixed_array& operator=(const fixed_array&) = delete;
+            fixed_array& operator=(fixed_array&& other) noexcept{
+                buf = std::exchange(other.buf,std::span<T>());
+                return *this;
+            }
+            T& operator[](std::size_t i) noexcept{
+                return buf[i];
+            }
+            const T& operator[](std::size_t i) const noexcept{
+                return buf[i];
+            }
+            T* data() noexcept{
+                return buf.data();
+            }
+            const T* data() const noexcept{
+                return buf.data();
+            }
+            std::size_t size() const noexcept{
+                return buf.size();
+            }
+            using iterator = T*;
+            using const_iterator = const T*;
+            T* begin() noexcept{
+                return buf.data();
+            }
+            const T* begin() const noexcept{
+                return buf.data();
+            }
+            T* end() noexcept{
+                return buf.data()+buf.size();
+            }
+            const T* end() const noexcept{
+                return buf.data()+buf.size();
+            }
+            ~fixed_array(){
+                delete[] buf.data();
+            }
+    };
 }
