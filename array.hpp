6a66fe7ba550313e25b08f4850770bca493a3d7e
maf <Programmer00001H@gmail.com>
2026-01-04 23:05:32 +0800

    array from range, value initialized array elements

--- array.hpp
+++ array.hpp
@@ -1,14 +1,21 @@
 #pragma once
 #include<cstdint>
 #include<utility>
+#include<ranges>
 #include<span>
 #include"iterable.hpp"
 namespace cppp{
+    constexpr inline struct value_init_tag_t{} value_init_tag;
     template<typename T>
     class fixed_array{
         std::span<T> buf;
         public:
+            template<std::ranges::sized_range R>
+            fixed_array(R&& ran) : fixed_array(std::ranges::size(ran)){
+                std::ranges::copy(ran,begin());
+            }
             fixed_array(std::size_t sz) : buf(new T[sz],sz){}
+            fixed_array(std::size_t sz,value_init_tag_t) : buf(new T[sz](),sz){}
             fixed_array(const fixed_array&) = delete;
             fixed_array(fixed_array&& other) noexcept : buf(std::exchange(other.buf,std::span<T>())){}
             fixed_array& operator=(const fixed_array&) = delete;
