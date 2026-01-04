18c527b9f201ee671c60727c0fb68b2729640063
maf <Programmer00001H@gmail.com>
2026-01-04 23:05:58 +0800

    binary: operator""_b, support r/w for signed ints

--- binary.hpp
+++ binary.hpp
@@ -8,6 +8,14 @@
 #include<climits>
 #include<span>
 namespace cppp{
+    inline namespace literals{
+        consteval inline std::byte operator ""_b(unsigned long long x){
+            return static_cast<std::byte>(x);
+        }
+        consteval inline std::byte operator ""_b(char c){
+            return static_cast<std::byte>(c);
+        }
+    }
     using buffer = std::span<std::byte>;
     template<std::size_t l>
     using staticbuffer = std::span<std::byte,l>;
@@ -27,32 +35,42 @@ namespace cppp{
         return std::as_bytes(std::span<const char,ts.size()>(ts));
     }
     template<typename T>
-    concept is_uint = std::same_as<T,std::byte> || (std::is_integral_v<T> && std::is_unsigned_v<T>);
+    concept is_int = std::same_as<T,std::byte> || std::is_integral_v<T>;
     namespace detail{
-        template<typename T>
+        template<typename T,std::size_t last>
         struct _expandbytes{};
-        template<std::size_t... index>
-        struct _expandbytes<std::integer_sequence<std::size_t,index...>>{
+        template<std::size_t... index,std::size_t last>
+        struct _expandbytes<std::integer_sequence<std::size_t,index...>,last>{
             template<typename I>
             static void write(std::byte* memory,I number){
                 (..., (
                     memory[index] = static_cast<std::byte>(number >> (index*CHAR_BIT))
                 ));
+                memory[last] = static_cast<std::byte>(number >> (last*CHAR_BIT));
             }
             template<typename I>
             static I read(const std::byte* memory){
-                return (... | (
+                I low = (... | (
                     static_cast<I>(memory[index]) << (index*CHAR_BIT)
                 ));
+                if constexpr(std::is_signed_v<I>){
+                    return low | static_cast<I>((static_cast<I>(memory[last]&0b0111'1111_b)-static_cast<I>(memory[last]&0b1000'0000_b)) << (last*CHAR_BIT));
+                }else{
+                    return low | (static_cast<I>(memory[last]) << (last*CHAR_BIT));
+                }
             }
         };
     }
-    template<is_uint I>
+    template<is_int I>
     void write(std::byte* memory,I number){
-        detail::_expandbytes<std::make_index_sequence<sizeof(I)>>::template write<I>(memory,number);
+        if constexpr(std::is_signed_v<I>){
+            write<std::make_unsigned_t<I>>(memory,static_cast<std::make_unsigned_t<I>>(number));
+        }else{
+            detail::_expandbytes<std::make_index_sequence<sizeof(I)-1uz>,sizeof(I)-1uz>::template write<I>(memory,number);
+        }
     }
-    template<is_uint I>
+    template<is_int I>
     I read(const std::byte* memory){
-        return detail::_expandbytes<std::make_index_sequence<sizeof(I)>>::template read<I>(memory);
+        return detail::_expandbytes<std::make_index_sequence<sizeof(I)-1uz>,sizeof(I)-1uz>::template read<I>(memory);
     }
 }
