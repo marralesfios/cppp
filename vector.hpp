380a26ef3f6772a789ee545999d7cc5b90b3d0ee
maf <Programmer00001H@gmail.com>
2026-01-15 20:46:10 +0800

    <vector.hpp> rework (breaking changes)

--- vector.hpp
+++ vector.hpp
@@ -1,89 +1,139 @@
 #pragma once
 #include<utility>
+#include<cstdint>
+#include<array>
 namespace cppp{
+    namespace detail{
+        template<typename T,std::size_t>
+        using _repeat_for_pack_t = T;
+        template<typename T,std::size_t ...indices>
+        class vec{
+            public:
+                consteval static std::size_t size(){
+                    return sizeof...(indices);
+                }
+            private:
+                std::array<T,size()> m;
+            public:
+                vec() = default;
+                vec(_repeat_for_pack_t<T,indices>... v) noexcept : m{v...}{}
+                template<typename U> requires(std::constructible_from<T,U>)
+                explicit(!std::convertible_to<U,T>) vec(vec<U,indices...> conv) : vec(static_cast<T>(conv[indices])...){}
+                T& operator[](std::size_t ind) noexcept{
+                    return m[ind];
+                }
+                const T& operator[](std::size_t ind) const noexcept{
+                    return m[ind];
+                }
+                T& x(){
+                    return m[0uz];
+                }
+                const T& x() const{
+                    return m[0uz];
+                }
+                T& y() requires(size()>1){
+                    return m[1uz];
+                }
+                const T& y() const requires(size()>1){
+                    return m[1uz];
+                }
+                vec& operator+=(vec other){
+                    (..., (m[indices] += other[indices]));
+                    return *this;
+                }
+                vec operator+(this vec lhs,vec rhs){
+                    return lhs += rhs;
+                }
+                vec operator-() const{
+                    return {-m[indices]...};
+                }
+                vec& operator-=(vec other){
+                    (..., (m[indices] -= other[indices]));
+                    return *this;
+                }
+                vec operator-(this vec lhs,vec rhs){
+                    return lhs -= rhs;
+                }
+                vec operator*=(vec other){
+                    (..., (m[indices] *= other[indices]));
+                    return *this;
+                }
+                vec operator*(this vec lhs,vec rhs){
+                    return lhs *= rhs;
+                }
+                vec& operator/=(vec other){
+                    (..., (m[indices] /= other[indices]));
+                    return *this;
+                }
+                vec operator/(this vec lhs,vec rhs){
+                    return lhs /= rhs;
+                }
+                vec& operator+=(T scalar){
+                    (..., (m[indices] += scalar));
+                    return *this;
+                }
+                vec operator+(this vec lhs,T scalar){
+                    return lhs += scalar;
+                }
+                vec& operator-=(T scalar){
+                    (..., (m[indices] -= scalar));
+                    return *this;
+                }
+                vec operator-(this vec lhs,T scalar){
+                    return lhs -= scalar;
+                }
+                vec& operator*=(T scalar){
+                    (..., (m[indices] *= scalar));
+                    return *this;
+                }
+                vec operator*(this vec lhs,T scalar){
+                    return lhs *= scalar;
+                }
+                vec& operator/=(T scalar){
+                    (..., (m[indices] /= scalar));
+                    return *this;
+                }
+                vec operator/(this vec lhs,T scalar){
+                    return lhs /= scalar;
+                }
+                bool operator==(vec other) const{
+                    return (...&&(m[indices] == other[indices]));
+                }
+        };
+        template<typename T,typename U>
+        struct vec_from_int_seq{};
+        template<typename T,std::size_t ...n>
+        struct vec_from_int_seq<T,std::index_sequence<n...>>{
+            using type = vec<T,n...>;
+        };
+    }
+    template<typename T,std::size_t n>
+    using vec = detail::vec_from_int_seq<T,std::make_index_sequence<n>>::type;
     template<typename T>
-    struct vec2{
-        T x;
-        T y;
-        vec2() : x(0), y(0){}
-        vec2(T x,T y) : x(x), y(y){}
-        template<std::convertible_to<T> U>
-        vec2(vec2<U> conv) : x(conv.x), y(conv.y){}
-        vec2& operator+=(vec2 other){
-            x += other.x;
-            y += other.y;
-            return *this;
-        }
-        vec2 operator+(this vec2 lhs,vec2 rhs){
-            return lhs += rhs;
-        }
-        vec2& operator-=(vec2 other){
-            x -= other.x;
-            y -= other.y;
-            return *this;
-        }
-        vec2 operator-(this vec2 lhs,vec2 rhs){
-            return lhs -= rhs;
-        }
-        vec2& operator*=(vec2 other){
-            x *= other.x;
-            y *= other.y;
-            return *this;
-        }
-        vec2 operator*(this vec2 lhs,vec2 rhs){
-            return lhs *= rhs;
-        }
-        vec2& operator/=(vec2 other){
-            x /= other.x;
-            y /= other.y;
-            return *this;
-        }
-        vec2 operator/(this vec2 lhs,vec2 rhs){
-            return lhs /= rhs;
-        }
-        vec2& operator+=(T scalar){
-            x += scalar;
-            y += scalar;
-            return *this;
-        }
-        vec2 operator+(this vec2 lhs,T scalar){
-            return lhs += scalar;
-        }
-        vec2& operator-=(T scalar){
-            x -= scalar;
-            y -= scalar;
-            return *this;
-        }
-        vec2 operator-(this vec2 lhs,T scalar){
-            return lhs -= scalar;
-        }
-        vec2& operator*=(T scalar){
-            x *= scalar;
-            y *= scalar;
-            return *this;
-        }
-        vec2 operator*(this vec2 lhs,T scalar){
-            return lhs *= scalar;
-        }
-        vec2& operator/=(T scalar){
-            x /= scalar;
-            y /= scalar;
-            return *this;
-        }
-        vec2 operator/(this vec2 lhs,T scalar){
-            return lhs /= scalar;
-        }
-        bool operator==(vec2 other) const{
-            return x==other.x&&y==other.y;
-        }
-    };
+    using vec2 = vec<T,2uz>;
+    template<typename T>
+    using vec3 = vec<T,3uz>;
+    template<typename T>
+    using vec4 = vec<T,4uz>;
+    using fvec2 = vec2<float>;
+    using uvec2 = vec2<std::uint32_t>;
+    using ivec2 = vec2<std::int32_t>;
+    using svec2 = vec2<std::size_t>;
+    using fvec3 = vec3<float>;
+    using uvec3 = vec3<std::uint32_t>;
+    using ivec3 = vec3<std::int32_t>;
+    using svec3 = vec3<std::size_t>;
+    using fvec4 = vec4<float>;
+    using uvec4 = vec4<std::uint32_t>;
+    using ivec4 = vec4<std::int32_t>;
+    using svec4 = vec4<std::size_t>;
 }
 namespace std{
-    template<typename T>
-    struct hash<cppp::vec2<T>>{
-        constexpr size_t operator()(cppp::vec2<T> v) const noexcept{
+    template<typename T,std::size_t ...indices>
+    struct hash<cppp::detail::vec<T,indices...>>{
+        constexpr size_t operator()(cppp::detail::vec<T,indices...> v) const noexcept{
             hash<T> h;
-            return h(v.x)^(h(v.y)<<2);
+            return (... ^ (h(v[indices])<<(indices&1)));
         }
     };
 }
