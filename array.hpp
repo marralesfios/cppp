0ff4bd42a8c89e91d890dce8ecdd1c297bd7921f
maf <Programmer00001H@gmail.com>
2026-01-26 15:31:40 +0800

    array emplace ctor

--- array.hpp
+++ array.hpp
@@ -18,6 +18,8 @@ namespace cppp{
             fixed_array(T* m,std::size_t n) : buf(m,n){}
             fixed_array(std::size_t sz) : buf(new T[sz],sz){}
             fixed_array(std::size_t sz,value_init_tag_t) : buf(new T[sz](),sz){}
+            template<typename ...C>
+            fixed_array(std::size_t sz,std::in_place_t,C&& ...t) : buf(new T[sz](std::forward<T>(t)...),sz){}
             fixed_array(const fixed_array&) = delete;
             fixed_array(fixed_array&& other) noexcept : buf(std::exchange(other.buf,std::span<T>())){}
             fixed_array& operator=(const fixed_array&) = delete;
