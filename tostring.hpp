d1b77f2ce5cc17db18ac7c0e14e62bdd44679065
maf <Programmer00001H@gmail.com>
2026-02-03 11:08:40 +0800

    basic to string support

---
+++ tostring.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include"template-string.hpp"
+#include"string.hpp"
+namespace cppp{
+    template<typename T>
+    str to_u8string(T&& v){
+        return tou8(std::to_string(std::forward<T>(v)));
+    }
+    template<template_string pat,typename ...T>
+    str format(T&& ...v){
+        str ret;
+        std::format_to(std::back_inserter(ret),ts_charconv_to_sv<char,pat>,forward_to_stdstring<T>(v)...);
+        return ret;
+    }
+}
