4e2c08b770588d4ab8d517cfaa79838b8b2229f4
maf <Programmer00001H@gmail.com>
2026-02-11 16:50:39 +0800

    exchange: add constexpr and noexcept

--- exchange.hpp
+++ exchange.hpp
@@ -1,20 +1,21 @@
 #pragma once
+#include<type_traits>
 #include<utility>
 namespace cppp{
     namespace detail{
-        inline void shl(){}
+        constexpr inline void shl() noexcept{}
         template<typename T,typename U>
-        void shl(T& x,U&& y){
+        constexpr void shl(T& x,U&& y){
             x = std::forward<U>(y);
         }
         template<typename T,typename U,typename ...Etc> requires(sizeof...(Etc)>0uz)
-        void shl(T& x,U& y,Etc&& ...etc){
+        constexpr void shl(T& x,U& y,Etc&& ...etc) noexcept(noexcept(x = std::move(y)) && noexcept(shl(y,std::forward<Etc>(etc)...))){
             x = std::move(y);
             shl(y,std::forward<Etc>(etc)...);
         }
     }
     template<typename First,typename ...Rest>
-    First shl(First& first,Rest&& ...rest){
+    constexpr First shl(First& first,Rest&& ...rest) noexcept(std::is_nothrow_move_constructible_v<First> && noexcept(detail::shl(first,std::forward<Rest>(rest)...))){
         First old_value{std::move(first)};
         detail::shl(first,std::forward<Rest>(rest)...);
         return old_value;
