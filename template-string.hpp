94e7bbe4e2fc1afebdd8ab3edff832e9dc611f3d
maf <Programmer00001H@gmail.com>
2026-02-12 22:03:39 +0800

    expose type-generic template string

--- template-string.hpp
+++ template-string.hpp
@@ -7,70 +7,70 @@ namespace cppp{
         struct static_char_storage{
             constexpr static CharT mem[]{ch...};
         };
-        template<typename CharT,std::size_t l>
-        struct template_string{
-            CharT arr[l];
-            using iterator = CharT*;
-            using const_iterator = const CharT*;
-            constexpr template_string(const CharT(&a)[l]){
-                std::size_t i = l;
-                while(i--){
-                    arr[i] = a[i];
-                }
-            }
-            consteval std::size_t size() const{
-                return l-1uz;
-            }
-            template<typename OtherCt>
-            constexpr template_string<OtherCt,l> charconv() const{
-                OtherCt other_arr[l];
-                std::size_t i = l;
-                while(i--){
-                    other_arr[i] = static_cast<OtherCt>(arr[i]);
-                }
-                return other_arr;
-            }
-            constexpr std::basic_string_view<CharT> sv() const{
-                return {arr,size()};
-            }
-            constexpr const CharT& operator[](std::size_t idx) const{
-                return arr[idx];
-            }
-            constexpr CharT& operator[](std::size_t idx){
-                return arr[idx];
-            }
-            constexpr const CharT& front() const{
-                return *arr;
-            }
-            constexpr CharT& front(){
-                return *arr;
-            }
-            constexpr const CharT& back() const{
-                return arr[size()-1];
-            }
-            constexpr CharT& back(){
-                return arr[size()-1];
-            }
-            constexpr char* begin(){
-                return arr;
-            }
-            constexpr char* end(){
-                return arr+size();
-            }
-            constexpr const char* begin() const{
-                return arr;
-            }
-            constexpr const char* end() const{
-                return arr+size();
-            }
-        };
     }
+    template<typename CharT,std::size_t l>
+    struct basic_template_string{
+        CharT arr[l];
+        using iterator = CharT*;
+        using const_iterator = const CharT*;
+        constexpr basic_template_string(const CharT(&a)[l]){
+            std::size_t i = l;
+            while(i--){
+                arr[i] = a[i];
+            }
+        }
+        consteval std::size_t size() const{
+            return l-1uz;
+        }
+        template<typename OtherCt>
+        constexpr basic_template_string<OtherCt,l> charconv() const{
+            OtherCt other_arr[l];
+            std::size_t i = l;
+            while(i--){
+                other_arr[i] = static_cast<OtherCt>(arr[i]);
+            }
+            return other_arr;
+        }
+        constexpr std::basic_string_view<CharT> sv() const{
+            return {arr,size()};
+        }
+        constexpr const CharT& operator[](std::size_t idx) const{
+            return arr[idx];
+        }
+        constexpr CharT& operator[](std::size_t idx){
+            return arr[idx];
+        }
+        constexpr const CharT& front() const{
+            return *arr;
+        }
+        constexpr CharT& front(){
+            return *arr;
+        }
+        constexpr const CharT& back() const{
+            return arr[size()-1];
+        }
+        constexpr CharT& back(){
+            return arr[size()-1];
+        }
+        constexpr char* begin(){
+            return arr;
+        }
+        constexpr char* end(){
+            return arr+size();
+        }
+        constexpr const char* begin() const{
+            return arr;
+        }
+        constexpr const char* end() const{
+            return arr+size();
+        }
+    };
     template<std::size_t n>
-    using c_template_string = detail::template_string<char,n>;
+    using c_template_string = basic_template_string<char,n>;
     template<std::size_t n>
-    using template_string = detail::template_string<char8_t,n>;
+    using template_string = basic_template_string<char8_t,n>;
     
-    template<typename CharU,detail::template_string ts>
+    template<typename CharU,basic_template_string ts>
     constexpr std::basic_string_view<CharU> ts_charconv_to_sv = []<std::size_t ...i>(std::index_sequence<i...>){
             return std::basic_string_view<CharU>(detail::static_char_storage<CharU,static_cast<CharU>(ts[i])...>{}.mem,ts.size());
         }(std::make_index_sequence<ts.size()>{});
