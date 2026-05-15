14455d2458b98d258cee2e722424d360337531e6
maf <Programmer00001H@gmail.com>
2026-05-15 20:43:36 +0800

    template-inheritance: rename and fix is_derived_from_instantiation(_v), and add a concept version

--- template-inheritance.hpp
+++ template-inheritance.hpp
@@ -2,13 +2,15 @@
 #include<concepts>
 namespace cppp{
     template<typename T,template<typename...> typename Te>
-    struct derived_from_instantiation{
+    struct is_derived_from_instantiation{
         constexpr static bool value = false;
     };
     template<template<typename...> typename Te,typename ...Values,std::derived_from<Te<Values...>> cls>
-    struct derived_from_instantiation<cls,Te>{
+    struct is_derived_from_instantiation<cls,Te>{
         constexpr static bool value = true;
     };
     template<typename T,template<typename...> typename Te>
-    using derived_from_instantiation_v = derived_from_instantiation::value;
+    constexpr static bool is_derived_from_instantiation_v = is_derived_from_instantiation::value;
+    template<typename T,template<typename...> typename Te>
+    concept derived_from_instantiation = is_derived_from_instantiation_v<T,Te>;
 }
