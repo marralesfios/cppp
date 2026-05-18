c4ea33c2882c8c50d896a6a7e118ab2a4961d30f
maf <Programmer00001H@gmail.com>
2026-05-18 14:19:32 +0800

    rename tostring to format and update to the new template strings implementation

--- format.hpp
+++ format.hpp
@@ -7,9 +7,15 @@ namespace cppp{
     str to_u8string(T&& v){
         return tou8(std::to_string(std::forward<T>(v)));
     }
+    namespace detail{
+        template<c_template_string pat,typename ...T>
+        void format_to(str& dst,T&& ...v){
+            std::format_to(std::back_inserter(dst),pat.sv(),forward_to_stdstring<T>(v)...);
+        }
+    }
     template<template_string pat,typename ...T>
     void format_to(str& dst,T&& ...v){
-        std::format_to(std::back_inserter(dst),ts_charconv_to_sv<char,pat>,forward_to_stdstring<T>(v)...);
+        detail::format_to<pat.template charconv<char>()>(dst,std::forward<T>(v)...);
     }
     template<template_string pat,typename ...T>
     str format(T&& ...v){
