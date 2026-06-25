19fc6c87dfead782d163d238fa7918eab055ba2f
maf <marralesfios@gmail.com>
2026-06-26 02:22:16 +0800

    stringify-enum: general improvements

--- stringify-enum.hpp
+++ stringify-enum.hpp
@@ -5,8 +5,17 @@ namespace cppp{
     template<typename EnumT>
     constexpr cppp::sv stringify_enum(EnumT v){
         template for(constexpr auto enumerator : define_static_array(enumerators_of(^^EnumT))){
-            if(v == extract<EnumT>(enumerator)){
+            if(v == [:enumerator:]){
                 return u8identifier_of(enumerator);
+            }
+        }
+        return {};
+    }
+    template<typename EnumT>
+    constexpr std::string_view c_stringify_enum(EnumT v){
+        template for(constexpr auto enumerator : define_static_array(enumerators_of(^^EnumT))){
+            if(v == [:enumerator:]){
+                return identifier_of(enumerator);
             }
         }
         return {};
