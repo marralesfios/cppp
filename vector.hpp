1eeb897b8a51618ca2a34cdad1ccd2b22ff0f0c5
maf <marralesfios@gmail.com>
2026-07-03 15:47:35 +0800

    vector: overhaul class structure to prettify underlying type names, and enable deduction of things like template<typename T> vec3<T>

--- vector.hpp
+++ vector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include<utility>
 #include<cstdint>
+#include<ranges>
 #include<array>
 #include<cmath>
 namespace cppp{
@@ -11,113 +12,142 @@ namespace cppp{
         constexpr T _repeat_for_pack_v(T v){
             return v;
         }
+        template<typename T,typename Inds>
+        class vec_base{};
         template<typename T,std::size_t ...indices>
-        class vec{
-            public:
-                consteval static std::size_t size(){
-                    return sizeof...(indices);
-                }
-            private:
-                std::array<T,size()> m;
-            public:
-                constexpr vec() noexcept = default;
-                constexpr vec(T v) noexcept : m{_repeat_for_pack_v<T,indices>(v)...}{}
-                constexpr vec(_repeat_for_pack_t<T,indices>... v) noexcept : m{v...}{}
+        class vec_base<T,std::index_sequence<indices...>>{
+            protected:
+                std::array<T,sizeof...(indices)> m;
                 template<typename U> requires(std::constructible_from<T,U>)
-                constexpr explicit(!std::convertible_to<U,T>) vec(vec<U,indices...> conv) : vec(static_cast<T>(conv[indices])...){}
-                constexpr T& operator[](std::size_t ind) noexcept{
-                    return m[ind];
-                }
-                constexpr const T& operator[](std::size_t ind) const noexcept{
-                    return m[ind];
-                }
-                constexpr T* data() noexcept{
-                    return m.data();
-                }
-                constexpr const T* data() const noexcept{
-                    return m.data();
-                }
-                constexpr T& x() noexcept{
-                    return m[0uz];
-                }
-                constexpr const T& x() const noexcept{
-                    return m[0uz];
-                }
-                constexpr T& y() noexcept requires(size()>1){
-                    return m[1uz];
-                }
-                constexpr const T& y() const noexcept requires(size()>1){
-                    return m[1uz];
-                }
-                constexpr T& z() noexcept requires(size()>2){
-                    return m[2uz];
-                }
-                constexpr const T& z() const noexcept requires(size()>2){
-                    return m[2uz];
-                }
-                constexpr T& w() noexcept requires(size()>3){
-                    return m[3uz];
-                }
-                constexpr const T& w() const noexcept requires(size()>3){
-                    return m[3uz];
-                }
-                constexpr vec xproj() const noexcept requires(size()>1){
-                    return vec((indices == 0uz ? x() : T())...);
-                }
-                constexpr vec yproj() const noexcept requires(size()>1){
-                    return vec((indices == 1uz ? y() : T())...);
-                }
-                constexpr vec zproj() const noexcept requires(size()>2){
-                    return vec((indices == 2uz ? z() : T())...);
-                }
-                constexpr vec& operator+=(vec other) noexcept{
-                    (..., (m[indices] += other[indices]));
-                    return *this;
-                }
-                constexpr vec operator-() const noexcept{
-                    return {-m[indices]...};
-                }
-                constexpr vec& operator-=(vec other) noexcept{
-                    (..., (m[indices] -= other[indices]));
-                    return *this;
-                }
-                constexpr vec operator*=(vec other) noexcept{
-                    (..., (m[indices] *= other[indices]));
-                    return *this;
-                }
-                constexpr vec& operator/=(vec other) noexcept{
-                    (..., (m[indices] /= other[indices]));
-                    return *this;
-                }
-                constexpr bool operator==(vec other) const noexcept{
-                    return (...&&(m[indices] == other[indices]));
-                }
-        };
-        template<typename T,std::size_t ...indices>
-        constexpr vec<T,indices...> operator+(vec<T,indices...> lhs,vec<T,indices...> rhs) noexcept{
-            return lhs += rhs;
-        }
-        template<typename T,std::size_t ...indices>
-        constexpr vec<T,indices...> operator-(vec<T,indices...> lhs,vec<T,indices...> rhs) noexcept{
-            return lhs -= rhs;
-        }
-        template<typename T,std::size_t ...indices>
-        constexpr vec<T,indices...> operator*(vec<T,indices...> lhs,vec<T,indices...> rhs) noexcept{
-            return lhs *= rhs;
-        }
-        template<typename T,std::size_t ...indices>
-        constexpr vec<T,indices...> operator/(vec<T,indices...> lhs,vec<T,indices...> rhs) noexcept{
-            return lhs /= rhs;
-        }
-        template<typename T,typename U>
-        struct vec_from_int_seq{};
-        template<typename T,std::size_t ...n>
-        struct vec_from_int_seq<T,std::index_sequence<n...>>{
-            using type = vec<T,n...>;
+                constexpr vec_base(std::array<U,sizeof...(indices)> conv) noexcept : vec_base(static_cast<T>(conv[indices])...){}
+            public:
+                constexpr vec_base() noexcept = default;
+                constexpr vec_base(T v) noexcept : m{_repeat_for_pack_v<T,indices>(v)...}{}
+                constexpr vec_base(_repeat_for_pack_t<T,indices>... v) noexcept : m{v...}{}
         };
     }
     template<typename T,std::size_t n>
-    using vec = detail::vec_from_int_seq<T,std::make_index_sequence<n>>::type;
+    class vec : detail::vec_base<T,std::make_index_sequence<n>>{
+        using indices_t = std::make_index_sequence<n>;
+        using base_t = detail::vec_base<T,indices_t>;
+        constexpr std::array<T,n> underlying_array() const noexcept{
+            return base_t::m;
+        }
+        template<typename U,std::size_t m>
+        friend class vec;
+        public:
+            consteval static std::size_t size(){
+                return n;
+            }
+            using base_t::vec_base;
+            template<typename U> requires(std::constructible_from<T,U>)
+            constexpr explicit(!std::convertible_to<U,T>) vec(vec<U,n> conv) noexcept : base_t(conv.underlying_array()){}
+            constexpr T& operator[](std::size_t ind) noexcept{
+                return base_t::m[ind];
+            }
+            constexpr const T& operator[](std::size_t ind) const noexcept{
+                return base_t::m[ind];
+            }
+            constexpr T* data() noexcept{
+                return base_t::m.data();
+            }
+            constexpr const T* data() const noexcept{
+                return base_t::m.data();
+            }
+            constexpr T& x() noexcept{
+                return base_t::m[0uz];
+            }
+            constexpr const T& x() const noexcept{
+                return base_t::m[0uz];
+            }
+            constexpr T& y() noexcept requires(n>1){
+                return base_t::m[1uz];
+            }
+            constexpr const T& y() const noexcept requires(n>1){
+                return base_t::m[1uz];
+            }
+            constexpr T& z() noexcept requires(n>2){
+                return base_t::m[2uz];
+            }
+            constexpr const T& z() const noexcept requires(n>2){
+                return base_t::m[2uz];
+            }
+            constexpr T& w() noexcept requires(n>3){
+                return base_t::m[3uz];
+            }
+            constexpr const T& w() const noexcept requires(n>3){
+                return base_t::m[3uz];
+            }
+            constexpr vec xproj() const noexcept requires(n>1){
+                constexpr auto [...indices] = indices_t{};
+                return vec((indices == 0uz ? x() : T())...);
+            }
+            constexpr vec yproj() const noexcept requires(n>1){
+                constexpr auto [...indices] = indices_t{};
+                return vec((indices == 1uz ? y() : T())...);
+            }
+            constexpr vec zproj() const noexcept requires(n>2){
+                constexpr auto [...indices] = indices_t{};
+                return vec((indices == 2uz ? z() : T())...);
+            }
+            constexpr vec& operator+=(vec other) noexcept{
+                template for(constexpr std::size_t index : std::views::indices(n)){
+                    base_t::m[index] += other[index];
+                }
+                return *this;
+            }
+            constexpr vec operator-() const noexcept{
+                constexpr auto [...indices] = indices_t{};
+                return {-base_t::m[indices]...};
+            }
+            constexpr vec& operator-=(vec other) noexcept{
+                template for(constexpr std::size_t index : std::views::indices(n)){
+                    base_t::m[index] -= other[index];
+                }
+                return *this;
+            }
+            constexpr vec operator*=(vec other) noexcept{
+                template for(constexpr std::size_t index : std::views::indices(n)){
+                    base_t::m[index] *= other[index];
+                }
+                return *this;
+            }
+            constexpr vec& operator/=(vec other) noexcept{
+                template for(constexpr std::size_t index : std::views::indices(n)){
+                    base_t::m[index] /= other[index];
+                }
+                return *this;
+            }
+    };
+    #define CPPP_GENERATE_VECTOR_OVERLOADS_WITH_RET(ret,name,...) template<typename T,std::size_t n> constexpr ret name (vec<T,n> lhs,vec<T,n> rhs) noexcept __VA_ARGS__  template<typename T,std::size_t n> constexpr ret name (T lhs,vec<T,n> rhs) noexcept __VA_ARGS__  template<typename T,std::size_t n> constexpr ret name (vec<T,n> lhs,T rhs) noexcept __VA_ARGS__
+    #define CPPP_COMMA ,
+    #define CPPP_GENERATE_VECTOR_OVERLOADS(name,...) CPPP_GENERATE_VECTOR_OVERLOADS_WITH_RET(vec<T CPPP_COMMA n>,name,__VA_ARGS__)
+    namespace detail{
+        template<typename T>
+        constexpr T index_vec(T v,std::size_t) noexcept{
+            return v;
+        }
+        template<typename T,std::size_t n>
+        constexpr T index_vec(vec<T,n> v,std::size_t ind) noexcept{
+            return v[ind];
+        }
+    }
+    CPPP_GENERATE_VECTOR_OVERLOADS_WITH_RET(bool,operator==,{
+        constexpr auto [...indices] = std::make_index_sequence<n>{};
+        return (...&&(index_vec(lhs,indices) == index_vec(rhs,indices)));
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(operator+,{
+        return vec<T,n>(lhs) += rhs;
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(operator-,{
+        return vec<T,n>(lhs) -= rhs;
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(operator*,{
+        return vec<T,n>(lhs) *= rhs;
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(operator/,{
+        return vec<T,n>(lhs) /= rhs;
+    })
     template<typename T>
     using vec2 = vec<T,2uz>;
     template<typename T>
@@ -136,23 +166,27 @@ namespace cppp{
     using uvec4 = vec4<std::uint32_t>;
     using ivec4 = vec4<std::int32_t>;
     using svec4 = vec4<std::size_t>;
-    template<typename T,std::size_t ...indices>
-    detail::vec<T,indices...> fmod(detail::vec<T,indices...> lhs,detail::vec<T,indices...> rhs){
-        return {std::fmod(lhs[indices],rhs[indices])...};
-    }
-    template<typename T,std::size_t ...indices>
-    detail::vec<T,indices...> max(detail::vec<T,indices...> lhs,detail::vec<T,indices...> rhs){
-        return {std::max(lhs[indices],rhs[indices])...};
-    }
-    template<typename T,std::size_t ...indices>
-    detail::vec<T,indices...> min(detail::vec<T,indices...> lhs,detail::vec<T,indices...> rhs){
-        return {std::min(lhs[indices],rhs[indices])...};
-    }
+    CPPP_GENERATE_VECTOR_OVERLOADS(fmod,{
+        constexpr auto [...indices] = std::make_index_sequence<n>{};
+        return {std::fmod(detail::index_vec(lhs,indices),detail::index_vec(rhs,indices))...};
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(max,{
+        constexpr auto [...indices] = std::make_index_sequence<n>{};
+        return {std::max(detail::index_vec(lhs,indices),detail::index_vec(rhs,indices))...};
+    })
+    CPPP_GENERATE_VECTOR_OVERLOADS(min,{
+        constexpr auto [...indices] = std::make_index_sequence<n>{};
+        return {std::min(detail::index_vec(lhs,indices),detail::index_vec(rhs,indices))...};
+    })
+    #undef CPPP_GENERATE_VECTOR_OVERLOADS
+    #undef CPPP_COMMA
+    #undef CPPP_GENERATE_VECTOR_OVERLOADS_WITH_RET
 }
 namespace std{
-    template<typename T,std::size_t ...indices>
-    struct hash<cppp::detail::vec<T,indices...>>{
-        constexpr size_t operator()(cppp::detail::vec<T,indices...> v) const noexcept{
+    template<typename T,std::size_t n>
+    struct hash<cppp::vec<T,n>>{
+        constexpr size_t operator()(cppp::vec<T,n> v) const noexcept{
+            constexpr auto [...indices] = std::make_index_sequence<n>{};
             hash<T> h;
             return (... ^ (h(v[indices])<<(indices&1)));
         }
