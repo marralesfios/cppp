dfdd853297802dcd0ad2e5bf00a1b6557cfddb68
maf <marralesfios@gmail.com>
2026-07-17 04:53:32 +0800

    assert: more robust and portable errors

--- assert.hpp
+++ assert.hpp
@@ -1,7 +1,40 @@
 #pragma once
 #ifdef NDEBUG
+#include<utility>
+#if __has_cpp_attribute(assume)
 #define CPPP_ASSERT(...) [[assume(__VA_ARGS__)]]
 #else
-#include<cassert>
-#define CPPP_ASSERT(...) assert((__VA_ARGS__))
+#define CPPP_ASSERT(...) do{ if(!(__VA_ARGS__)) std::unreachable(); }while(0)
+#endif
+namespace cppp{
+    [[noreturn]] constexpr inline void unreachable() noexcept{
+        std::unreachable();
+    }
+}
+#else
+#include<source_location>
+#include<iostream>
+#include"string.hpp"
+#define CPPP_ASSERT(...) ::cppp::assert_true((__VA_ARGS__),::cppp::detail::view_u8array(u8 ## #__VA_ARGS__))
+
+namespace cppp{
+    namespace detail{
+        template<std::size_t n>
+        std::u8string_view view_u8array(const char8_t(&arr)[n]){
+            return std::u8string_view(arr,n);
+        }
+    }
+    constexpr inline void assert_true(bool value,std::u8string_view text,std::source_location where=std::source_location::current()){
+        if(!value){
+            using namespace std::literals;
+            std::cerr << "Assertion '"sv << cview(text) << "' failed in function "sv << where.function_name() << "at "sv << where.file_name() << ':' << where.line() << ':' << where.column() << '\n';
+            std::abort();
+        }
+    }
+    [[noreturn]] constexpr inline void unreachable(std::source_location where=std::source_location::current()) noexcept{
+        using namespace std::literals;
+        std::cerr << "Unreachable executed in function "sv << where.function_name() << "at "sv << where.file_name() << ':' << where.line() << ':' << where.column() << '\n';
+        std::abort();
+    }
+}
 #endif
