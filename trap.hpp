32a4d9847ebabbc553908108a6165983cacaf88b
maf <marralesfios@gmail.com>
2026-08-17 14:22:51 +0800

    don't rely on IB for consteval trap

--- trap.hpp
+++ trap.hpp
@@ -1,9 +1,9 @@
 #pragma once
 namespace cppp{
-    [[noreturn]] consteval void consteval_trap(){
-        [[assume(false)]]; // UB disallowed at compile time
+    [[noreturn]] consteval void consteval_trap() noexcept{
+        throw 0;
     }
-    consteval void consteval_assert(bool v){
+    consteval void consteval_assert(bool v) noexcept{
         if(!v) consteval_trap();
     }
 }
