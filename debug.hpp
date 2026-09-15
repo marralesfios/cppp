64d3b246a24a57ba9f49ebab95a775935f5b2866
maf <marralesfios@gmail.com>
2026-09-15 18:51:42 +0800

    debug: debug enums

--- debug.hpp
+++ debug.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include"stringify-enum.hpp"
 #include"string.hpp"
 #include"print.hpp"
+#include<type_traits>
 #include<concepts>
 #include<iostream>
 #include<ranges>
@@ -22,6 +24,11 @@ namespace cppp{
         }
         inline void debug_item(std::string_view s){
             print<u8"{}"_ts>(std::clog,s);
+        }
+        template<typename E> requires(std::is_enum_v<E>)
+        inline void debug_item(E ev){
+            using namespace std::literals;
+            print<u8"{}"_ts>(std::clog,u8identifier_of(^^E)+u8"::"s+stringify_enum(ev));
         }
         template<typename ...T>
         void debug(T&& ...v){
